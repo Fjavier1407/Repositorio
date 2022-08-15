d1b4f43fd01777a14358726055e37d84c21b5d0e
Fjavier1407 <fernandojavier1407@gmail.com>
2022-08-15 19:43:59 -0300

    primer commit del proyecto
    Declaracion de Clases a utilizar

---
+++ PreEntrega_15_08/PreEntrega_15_08/Program.cs
@@ -0,0 +1,44 @@
+﻿public class Producto
+{
+    private long idProducto;
+    private string descripcion;
+    private double costo;
+    private double precioVenta;
+    private int stock;
+    private long idUsuario;
+}
+
+public class Usuario
+{
+    private long idUsuario;
+    private string nombre;
+    private string apellido;
+    private string NombreUsuario;
+    private string contraseña;
+    private string mail;
+}
+
+public class Producto
+{
+    private long idProducto;
+    private string descripcion;
+    private double costo;
+    private double precioVenta;
+    private int stock;
+    private long idUsuario;
+}
+
+public class ProductoVendido
+{
+    private long idProductoVendido;
+    private long idProducto
+    private int stock;
+    private idVenta
+}
+
+public class Venta
+{
+    private long idVenta;
+    private string comentarios;
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mi_primer_repositorio
+
+1-Declaracion de las clases a usar en el proyecto
